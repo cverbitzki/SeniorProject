8930356ae0b16c5eb91dcbb2785e4a0b906078b7
Jurden <jordan.millett@maine.edu>
2016-10-10 12:11:11 -0400

    Added SPI files

---
+++ HAT/SPI/spi.c
@@ -0,0 +1,5 @@
+/*	spi.c	*/
+/*	Jordan Millett */
+/*	8 OCT 2016	*/
+
+#include <avr/io.h>     // For AVR registers
---
+++ HAT/SPI/spi.h
@@ -0,0 +1,8 @@
+/*	triac.h	*/
+/*	Jordan Millett */
+/*	8 OCT 2016	*/
+
+#ifndef SPI_H
+#define SPI_H
+
+#endif SPI_H
