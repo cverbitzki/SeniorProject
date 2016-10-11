2f1554a82ee1a9f39194571a023926de6525761a
Jurden <jordan.millett@maine.edu>
2016-10-11 12:27:54 -0400

    SPI files

--- HAT/SPI/spi.c
+++ HAT/SPI/spi.c
@@ -3,3 +3,40 @@
 /*	8 OCT 2016	*/
 
 #include <avr/io.h>     // For AVR registers
+
+/* Master - ATMEGA
+ * PB4 MISO
+ * PB4 MOSI
+ * PB2 SS
+ * PB5 SCK
+ */
+
+ /* Slave - ATTINY
+ * PB4 MISO
+ * PB4 MOSI
+ * PB2 SS
+ * PB5 SCK
+ */
+
+void spi_init_master(void)
+{
+
+	SPCR = ((1 << SPE) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA));
+}
+
+void spi_init_slave(void)
+{
+	SPCR = (1 << MSTR);
+}
+
+/*	Transmits 8 bit data 	*/
+/*	Returns recieved 8 bit data 	*/
+uint8_t spi_transmit(uint8_t data)
+{
+	/*	Set SPI data buffer to data	*/
+	SPDR = data;
+	/*	Wait for completed flag	*/
+	while (!(SPSR & (1 << SPIF)));
+	/* Return recieved data	*/
+	return SPDR;
+}
--- HAT/SPI/spi.h
+++ HAT/SPI/spi.h
@@ -5,4 +5,11 @@
 #ifndef SPI_H
 #define SPI_H
 
+void spi_init_master(
+	void
+	);
+void spi_init_slave(
+	void
+	);
+
 #endif SPI_H
