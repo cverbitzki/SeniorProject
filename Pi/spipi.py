82029cc0adc8e87c08528af3cad362e70a8d0686
Jurden <jordan.millett@maine.edu>
2016-10-16 17:41:45 -0400

    Working spi on pi

--- Pi/spipi.py
+++ Pi/spipi.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import pigpio
+import time
 
 spi_chan = 0
-baud = 8000000
+baud = 800000
 
 pi = pigpio.pi()
 
@@ -13,8 +14,13 @@ pi.set_mode(23, pigpio.OUTPUT)
 slave = pi.spi_open(spi_chan, baud, 0)
 
 send = "A"
-recieve = "B"
+recieve = ""
 while 1:
-	pi.spi_xfer(slave, send, recieve, 1)
-	print(recieve)
+	(count, recieve) = pi.spi_xfer(slave, send)
+#	if (recieve != "B"):
+#		continue
 	
+	print(send)
+	print(recieve)
+	time.sleep(1)
+	send = chr(ord(send) + 1)
