fff5ae5563158817c6068a1e8b1b7096634d8847
Jurden <jordan.millett@maine.edu>
2016-11-10 11:49:48 -0500

    Work on spi routine

--- HAT/inc/spi.c
+++ HAT/inc/spi.c
@@ -1,11 +1,18 @@
 /*	spi.c	*/
 /*	Jordan Millett */
 /*	8 OCT 2016	*/
-
 #include <avr/io.h>     // For AVR registers
 #include <avr/interrupt.h>	
 #include <stdint.h>
+#include "hat_eeprom.h"
 
+#define ERROR "E"
+
+/* Spi status register, one byte
+ * 0	0	0	0		0	0	0	0	 
+ * na	na	na	na		Passcode index:
+ * 						Current digit pi is requesting
+ */
 
 void spi_slave_init(void)
 {
@@ -15,8 +22,8 @@ void spi_slave_init(void)
 	DDRB = (1 << PB4);
 	/*	Enable spi and interrupt	*/
 	SPCR = ((1 << SPE) | (1 << SPIE));
+	/* Clear data register 	*/
 	SPDR = 0;
-	
 	/* Reenable interrupts 	*/
 	sei();
 }
@@ -32,3 +39,37 @@ void spi_transmit(char data)
 //	while(!(SPSR & (1 << SPIF)));
 }
 
+spi_send_pass() {
+	char status;
+	char pass[4];
+	/* Get passkey from eeprom 	*/
+	get_pass(pass);
+	/* Get spi status 	*/
+	status = spi_get_data();
+	/* Find which digit to be sent next 	*/
+	switch (status & 15) {
+	case 1:
+		spi_transmit(pass[0]);
+		/* Clear bit 1, set bit 2 	*/
+		spi_write_data(status ^= 3);
+		break;
+	case 2:
+		spi_transmit(pass[1]);
+		/* Clear bit 2, set bit 3 	*/
+		spi_write_data(status ^= 6);
+		break;
+	case 4:
+		spi_transmit(pass[2]);
+		/* Clear bit 3, set bit 4 	*/
+		spi_write_data(status ^= 12);
+		break;
+	case 8:
+		spi_transmit(pass[3]);
+		/* Clear bits 1-4 	*/
+		spi_write_data(status &= 8);
+		break;
+	default:
+		spi_transmit(ERROR);	
+		break;
+	}
+}
--- HAT/main.c
+++ HAT/main.c
@@ -37,48 +37,29 @@
 int main(void)
 {
 
-	char input;
+	char spi_dat;
 	/* Set up spi 	*/
 	spi_slave_init();
 	/* Transmit ready	*/
 	spi_transmit('R');
 	/* Enable interrupts, just in case	*/
+	spi_get_data()
 	sei();
     while(1) {
-	/* Wait for interrupt 	*/     
+		/* Wait for interrupt 	*/     
     }     
 }
 
 /* SPI Interrupt routine 	*/
 ISR(SPI_STC_vect)
 {
+	char data;
 	/* Disable interrupts 	*/
 	cli();
 	
-	char data;
-
-	toggle_pin(PORTB, 1);
-	
 	/* Get data from register 	*/
-	data = spi_recieve();
+	spi_write_data(spi_recieve());
 
-	if (data == 'A') {
-		data = 'B';
-	} else {
-		data = 'Z';
-	}
-
-	/* send data over serial 	*/
-	spi_transmit(data);
-
-	/* Save data to eeprom 	*/
-	//spi_write_data(data);
-	
-	/* Transmit byte	*/
-	//spi_transmit(data);
-	
-	//output_low(PORTB, 1);
-	
 	/* Reenable interrupts 	*/
 	sei();
 }
